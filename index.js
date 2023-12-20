ef021cc970b1f729a982a3f98619956f5b276fa3
serwerApache <154437721+serwerApache@users.noreply.github.com>
2023-12-20 18:15:03 +0100

    poprawa pokoi

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 app.get('/', (req, res) => {
     if (!req.cookies.id) res.cookie("id", nanoid(), { expire: 360000 + Date.now() });
-
+    res.cookie("roomID", nanoid())
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
@@ -26,11 +26,10 @@ app.get('/:file', (req, res) => {
 });
 
 app.post('/createRoom', (req, res) => {
-    const roomID = nanoid();
+    const roomID = req.cookies.roomID;
 
-    if (rooms.has(roomID)) {
-        return res.status(409).json({ error: 'Room already exists' });
-    }
+    if (rooms.has(roomID)) return
+
 
     const room = {
         users: new Map(),
@@ -44,9 +43,7 @@ app.post('/joinRoom', (req, res) => {
     const roomID = req.body.roomID;
     const socketID = req.body.socketID;
 
-    if (!rooms.has(roomID)) {
-        return res.status(404).json({ error: 'Room not found' });
-    }
+    if (!rooms.has(roomID)) return
 
     const room = rooms.get(roomID);
     room.users.set(socketID, users.get(socketID));
@@ -74,15 +71,20 @@ io.on('connection', socket => {
 
     socket.on('joinRoom', (roomID, userID) => {
         socket.join(roomID);
-        users.get(socket.id).roomID = roomID
 
+        const user = users.get(socket.id)
+        if (user.roomID != '' && user.roomID != roomID) {
+            //console.log(io.sockets.adapter.rooms.get(user.roomID))
+            socket.leave(user.roomID);
+            updateRooms(user.roomID);
+        }
+        user.roomID = roomID
         updateRooms(roomID)
     });
 
     socket.on('leaveRoom', (roomID, userID) => {
         socket.leave(roomID);
         users.get(socket.id).roomID = ''
-
         updateRooms(roomID)
     });
 
@@ -97,7 +99,7 @@ const updateRooms = roomID => {
     if (rooms.has(roomID)) {
         const roomUsers = rooms.get(roomID).users
         for (const key of roomUsers.keys()) {
-            if (!users.has(key)) {
+            if (!users.has(key) || !io.sockets.adapter.rooms.get(roomID)) {
                 rooms.get(roomID).users.delete(key)
                 if (roomUsers.size == 0) {
                     rooms.delete(roomID)
@@ -107,6 +109,10 @@ const updateRooms = roomID => {
         }
     }
 }
+
+setInterval(() => {
+    console.log(rooms)
+}, 500)
 
 server.listen(3000, () => {
     console.log('http://localhost:3000');
